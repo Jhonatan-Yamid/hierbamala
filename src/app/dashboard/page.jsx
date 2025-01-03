6956e1f63b75441e1c67583e7f8a6c13281ba198
Jhonatan <yamidjhonatan@gmail.com>
2025-01-03 15:51:04 -0500

    11

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -6,7 +6,7 @@ function DashboardPage() {
   return (
     <section className="h-[calc(100vh-7rem)] flex justify-center items-center">
       <div>
-        <h1 className="text-white text-5xl">Bienvenido {session?.user?.name} y {session?.user?.image} y {session?.user?.image ===1}</h1>
+        <h1 className="text-white text-5xl">Bienvenido {session?.user?.name} y {session?.user?.image} y {session?.user?.image !== 1 ? "true" : "false"} </h1>
         {/* <button className="bg-white text-black px-4 py-2 rounded-md mt-4"
           onClick={() => signOut()}
         >
