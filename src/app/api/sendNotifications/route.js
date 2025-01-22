35ed663cca5a0d3de7e6118429ab1d3af1116e5d
Jhonatan <yamidjhonatan@gmail.com>
2025-01-22 08:41:04 -0500

    sss

--- src/app/api/sendNotifications/route.js
+++ src/app/api/sendNotifications/route.js
@@ -1,4 +1,3 @@
-// src/app/api/jobs/sendNotifications/route.js
 import webPush from '@/libs/webPushConfig';
 import db from '@/libs/db';
 
@@ -7,6 +6,12 @@ export async function GET() {
     const now = new Date();
     const todayStart = new Date(now.getFullYear(), now.getMonth(), now.getDate());
     const todayEnd = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+
+    // Convertir fechas al formato colombiano
+    const nowFormatted = now.toLocaleString('es-CO', { timeZone: 'America/Bogota' });
+    const todayStartFormatted = todayStart.toLocaleString('es-CO', { timeZone: 'America/Bogota' });
+    const todayEndFormatted = todayEnd.toLocaleString('es-CO', { timeZone: 'America/Bogota' });
+    let alertassumadas = "";
 
     // Obtener alertas que coincidan con el día actual y si son semanales, verificar que coincidan con el día de la semana
     const alerts = await db.alert.findMany({
@@ -20,22 +25,28 @@ export async function GET() {
           },
           {
             repeatWeekly: true,
-            repeatDay: now.getDay(),  // Verificar si es el día de la semana correcto
+            repeatDay: now.getDay(), // Verificar si es el día de la semana correcto
           },
         ],
       },
     });
 
     if (alerts.length === 0) {
-      return new Response(JSON.stringify({ success: false, message: 'No hay alertas para hoy now:'+now+'todayStart'+todayStart+'todayEnd'+todayEnd }), {
-        status: 200,
-      });
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message: `No hay alertas para hoy. now: ${nowFormatted}, todayStart: ${todayStartFormatted}, todayEnd: ${todayEndFormatted}`,
+        }),
+        { status: 200 }
+      );
     }
 
     // Obtener suscripciones activas
     const subscriptions = await db.subscription.findMany();
+    
 
     alerts.forEach(async (alert) => {
+      alertassumadas += alert.title
       const notificationPayload = JSON.stringify({
         title: alert.title,
         body: alert.description,
@@ -63,13 +74,21 @@ export async function GET() {
       });
     });
 
-    return new Response(JSON.stringify({ success: true, message: 'Notificaciones enviadas now:'+now+'todayStart'+todayStart+'todayEnd'+todayEnd }), {
-      status: 200,
-    });
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: `Notificaciones enviadas. now: ${nowFormatted}, todayStart: ${todayStartFormatted}, todayEnd: ${todayEndFormatted} otro ${alertassumadas}`,
+      }),
+      { status: 200 }
+    );
   } catch (error) {
     console.error('Error en el proceso de notificación:', error);
-    return new Response(JSON.stringify({ success: false, error: error.message }), {
-      status: 500,
-    });
+    return new Response(
+      JSON.stringify({
+        success: false,
+        error: error.message,
+      }),
+      { status: 500 }
+    );
   }
 }
