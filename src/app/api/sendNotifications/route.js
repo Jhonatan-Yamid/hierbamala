16187cea6daa13b0b7eead492e48688b418285f6
Jhonatan <yamidjhonatan@gmail.com>
2025-01-24 08:56:59 -0500

    sss

--- src/app/api/sendNotifications/route.js
+++ src/app/api/sendNotifications/route.js
@@ -1,4 +1,3 @@
-import webPush from '@/libs/webPushConfig';
 import db from '@/libs/db';
 
 export async function GET() {
@@ -24,64 +23,18 @@ export async function GET() {
       },
     });
 
-    if (alerts.length === 0) {
-      return new Response(JSON.stringify({ success: false, message: 'No hay alertas para hoy', alerts }), { status: 200 });
-    }
-
     const subscriptions = await db.subscription.findMany();
-    const results = [];
-
-    for (const alert of alerts) {
-      const notificationPayload = JSON.stringify({
-        title: alert.title,
-        body: alert.description,
-      });
-
-      for (const subscription of subscriptions) {
-        try {
-          await webPush.sendNotification(
-            {
-              endpoint: subscription.endpoint,
-              keys: subscription.keys,
-            },
-            notificationPayload
-          ).catch((error) => {
-            console.log(error);
-            return new Response(
-              "error::"+error
-            );
-           
-          });;
-          results.push({ endpoint: subscription.endpoint, status: 'success'+alert.title });
-        } catch (error) {
-          console.error('Error al enviar notificación:', error);
-          results.push({
-            endpoint: subscription.endpoint,
-            status: 'error',
-            message: error.message,
-            statusCode: error.statusCode,
-          });
-
-          // Eliminar suscripción inválida
-          if (error.statusCode === 410 || error.statusCode === 404) {
-            await db.subscription.delete({
-              where: { endpoint: subscription.endpoint },
-            });
-          }
-        }
-      }
-    }
 
     return new Response(
       JSON.stringify({
         success: true,
-        message: 'Notificaciones procesadas',
-        results,
+        alerts,
+        subscriptions,
       }),
       { status: 200 }
     );
   } catch (error) {
-    console.error('Error general:', error);
+    console.error('Error obteniendo datos:', error);
     return new Response(
       JSON.stringify({
         success: false,
