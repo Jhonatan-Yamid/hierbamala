a286ee636d90e9027f45c2e9f0b1f67a45dca37d
Jhonatan <yamidjhonatan@gmail.com>
2025-01-24 07:34:30 -0500

    ssss

---
+++ src/app/api/apitest/route.js
@@ -0,0 +1,34 @@
+
+import webPush from 'web-push';
+webPush.setVapidDetails(
+    'mailto:yamidjhonatan@gmail.com',
+    process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
+    process.env.VAPID_PRIVATE_KEY
+);
+export async function GET() {
+    const pushSubscription = {
+        endpoint: 'https://fcm.googleapis.com/fcm/send/cDVpb0-IhMQ:APA91bEi5Z8kLbPbFOgvmQf-otmLKbEND8DP4KM3XirMxEXI-XqEpk6Rbfk9ifoff_xdHWCEsWMMyIhFG9dsg7nzzET0cTciO1zLqLII4Xnp1YubmqSjdUz6jc-kGSfCpUtRKGrg74Yc',
+        keys: {
+            auth: 'PUo9lzuX2XcM45lhqPAG1Q',
+            p256dh: 'BMpkUPWSnAtHz1XpZuJ6KZWezzM6G7o5PYaQ2NfZ2CqgfT7NPLGASEJXmqrQSDascTZnieoxnadzMHW8XA8PkAo'
+        }
+    };
+    const notificationPayload = JSON.stringify({
+        title: "test2024",
+        body: "funciona",
+    });
+    try {
+        webPush.sendNotification(pushSubscription, notificationPayload);
+        return new Response(
+            JSON.stringify({
+                success: true,
+                message: 'Notificaciones procesadas',
+            }),
+            { status: 200 }
+        );
+    } catch (error) {
+        return new Response(
+            error
+        );
+    }
+}
