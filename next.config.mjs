7fd9c1e343cbc92038c3515bf079833ca2766bc3
Jhonatan <yamidjhonatan@gmail.com>
2025-01-25 17:42:16 -0500

    ssss

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,21 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/api/(.*)', // Aplica a todas las rutas de API
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          { key: 'Pragma', value: 'no-cache' },
+          { key: 'Expires', value: '0' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
