41d1d47083cbea695dca9891ed1fa84cde53eecc
Jhonatan Alzate <yamidjhonatan@gmail.com>
2026-03-02 16:35:19 -0500

    feat(ProductSearch);Redeploy caching force

--- src/app/api/products/search/route.js
+++ src/app/api/products/search/route.js
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import db from "@/libs/db";
 
-
+export const dynamic = "force-dynamic";
 export async function GET() {
   try {
     const products = await db.product.findMany({
